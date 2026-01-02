5a56ae0e885870adb53c82c719d0c813a027dfe2
ncatt <anntuongg@gmail.com>
2026-01-02 13:57:21 +0700

    Update anykernel.sh

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=vampire for a32 | An Tuong @ xdaforums
+kernel.string=moonlight for a32 | An Tuong @ xdaforums
 do.devicecheck=1
 do.modules=0
 do.systemless=1
